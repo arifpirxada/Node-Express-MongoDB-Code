cb733a9c5ca71b06bfa5a1d54cffc582c6819118
arifpirxada <pirxadaarif@gmail.com>
2023-08-23 21:09:46 +0530

    learned path module and to create own modules

---
+++ Modules/path.js
@@ -0,0 +1,6 @@
+const path = require("path");
+
+console.log("Path: ",path.dirname("/home/adaxripfira/Documents/index.js"))
+console.log("Extension: ",path.extname("/home/adaxripfira/Documents/index.js"))
+console.log("Basename: ",path.basename("/home/adaxripfira/Documents/index.js"))
+console.log("Obj: ",path.parse("/home/adaxripfira/Documents/index.js"))
