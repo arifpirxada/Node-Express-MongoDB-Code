a3b15b26e857e38420325866b60505930b558c05
Arif <arifpirxada@gmail.com>
2023-08-24 00:57:54 +0530

    Fixed content-type

--- Node Web Server/index.js
+++ Node Web Server/index.js
@@ -7,7 +7,7 @@ const server = http.createServer((req, res) => {
     } else if(req.url == "/about") {
         res.end("Hi! About us page here")
     } else {
-        res.writeHead(404, "Content-type: text/html")
+        res.writeHead(404, {"content-type": "text/html"})
         res.end("<h1>404 Page does not exist</h1>")
     }
 })
