80c59a6c317a1c05b994fb4ea818777ec88a34c6
arifpirxada <pirxadaarif@gmail.com>
2023-08-23 19:11:54 +0530

    initial commit

---
+++ Modules/fsCrud.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+
+// fs.mkdirSync("thapa")
+
+// fs.writeFileSync("thapa/bio.txt","first data here,")
+
+// fs.appendFileSync("thapa/bio.txt","second data here")
+
+// const bioData = fs.readFileSync("thapa/bio.txt","utf-8")
+// console.log(bioData)
+
+// fs.renameSync("thapa/bio.txt","thapa/mybio.txt")
+
+// fs.unlinkSync("thapa/mybio.txt")
+// fs.rmdirSync("thapa")
