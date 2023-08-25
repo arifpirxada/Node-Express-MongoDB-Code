9795d04ad2e129b8bbf77529fb841500742f4b03
Arif <arifpirxada@gmail.com>
2023-08-25 17:21:44 +0530

    Partials in Express - hbs

--- Express/Template-Engine/src/index.js
+++ Express/Template-Engine/src/index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const app = express()
 const path = require('path');
+const hbs = require('hbs');
 
 const staticPath = path.join(__dirname, "../public")
 app.use(express.static(staticPath))
@@ -10,8 +11,14 @@ app.set("view engine", "hbs")
 /* The below method allows you to change the name 
 of hbs containing folder - views and it's path */
 
-// const templatePath = path.join(__dirname, "../template")
-// app.set("views", templatePath)
+const templatePath = path.join(__dirname, "../templates/views")
+app.set("views", templatePath)
+
+
+
+const partialsPath = path.join(__dirname, "../templates/partials")
+hbs.registerPartials(partialsPath)
+
 
 
 app.get("/", (req, res) => {
